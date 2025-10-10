cfdc1a6583920cf0023f69f910497e82bba97b6d
saurabh_sr <saurabh8599@gmail.com>
2025-10-10 14:05:51 -0400

    Doctor and patient get update api

--- src/main/java/com/example/wecureit_be/repository/PatientMasterRepository.java
+++ src/main/java/com/example/wecureit_be/repository/PatientMasterRepository.java
@@ -2,8 +2,13 @@ package com.example.wecureit_be.repository;
 
 import com.example.wecureit_be.entity.PatientMaster;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PatientMasterRepository extends JpaRepository<PatientMaster, String> {
+
+    @Query(value = "SELECT * FROM patient_master WHERE patient_master_id = :patientId", nativeQuery = true)
+    PatientMaster getPatientById(@Param("patientId") String patientId);
 }
