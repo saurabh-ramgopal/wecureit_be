107c1c433147a859e792e5fb2b4ee7465f8c77ba
ullasbc <ullasbc02@gmail.com>
2025-11-10 15:39:09 -0500

    Adding Card Response DTO and modifying CardController to use request parameters instead of path variables.

--- src/main/java/com/example/wecureit_be/controller/CardController.java
+++ src/main/java/com/example/wecureit_be/controller/CardController.java
@@ -3,11 +3,15 @@ package com.example.wecureit_be.controller;
 import com.example.wecureit_be.entity.Card;
 import com.example.wecureit_be.impl.CardService;
 import com.example.wecureit_be.request.CardRequest;
+import com.example.wecureit_be.response.CardResponse;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 import java.util.List;
 @RestController
-@RequestMapping("{patient_master_id}/cards")
+@RequestMapping("/cards")
 public class CardController {
 
     private final CardService cardService;
@@ -17,8 +21,11 @@ public class CardController {
     }
 
     @PostMapping("/add")
-    public Card addCard(@RequestBody CardRequest req) throws Exception {
-        return cardService.addCard(req);
+    public ResponseEntity<CardResponse> addCard(@RequestBody CardRequest req) throws Exception {
+        Card card = cardService.addCard(req);
+        URI location = URI.create("/cards/" + card.getId());
+        // return only id and last4 in response (see CardResponse)
+        return ResponseEntity.created(location).body(new CardResponse(card.getId(), card.getLast4()));
     }
 
     @GetMapping("/{id}")
@@ -33,7 +40,7 @@ public class CardController {
     }
 
     @GetMapping("/getcards")
-    public List<String> getCards(@PathVariable("patient_master_id") Integer patientMasterId) {
+    public List<String> getCards(@RequestParam(name = "patientId") Integer patientMasterId) {
         return cardService.getCardsByPatientId(patientMasterId);
     }
 
