2784798dd034c9c970f76422bbd3e4eef7687b64
ullasbc <ullasbc02@gmail.com>
2025-11-10 15:13:56 -0500

    Adding card entity and related components

--- src/main/java/com/example/wecureit_be/controller/CardController.java
+++ src/main/java/com/example/wecureit_be/controller/CardController.java
@@ -5,9 +5,9 @@ import com.example.wecureit_be.impl.CardService;
 import com.example.wecureit_be.request.CardRequest;
 
 import org.springframework.web.bind.annotation.*;
-
+import java.util.List;
 @RestController
-@RequestMapping("/cards")
+@RequestMapping("{patient_master_id}/cards")
 public class CardController {
 
     private final CardService cardService;
@@ -30,6 +30,11 @@ public class CardController {
     public String viewMaskedCard(@PathVariable Long id) throws Exception {
         String pan = cardService.viewCard(id);
         return "**** **** **** " + pan.substring(pan.length() - 4);
+    }
+
+    @GetMapping("/getcards")
+    public List<String> getCards(@PathVariable("patient_master_id") Integer patientMasterId) {
+        return cardService.getCardsByPatientId(patientMasterId);
     }
 
 
